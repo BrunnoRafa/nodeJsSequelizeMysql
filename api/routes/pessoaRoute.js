03f2862919fde236656846f7c13fc76c4c2d3ca6
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2021-04-17 23:15:00 -0300

    Criando CRUD de matriculas e implementando as rotas de niveis e turmas no index

--- api/routes/pessoaRoute.js
+++ api/routes/pessoaRoute.js
@@ -3,14 +3,15 @@ const PessoaController = require('../controllers/PessoaController');
 
 const router = Router();
 
-router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
-
-router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
-
-router.post('/pessoas', PessoaController.criarPessoa);
-
-router.put('/pessoas/:id', PessoaController.atualizaPessoa);
-
-router.delete('/pessoas/:id', PessoaController.apagaPessoa);
+router
+  .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+  .post('/pessoas', PessoaController.criarPessoa)
+  .put('/pessoas/:id', PessoaController.atualizaPessoa)
+  .delete('/pessoas/:id', PessoaController.apagaPessoa)
+  .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+  .post('/pessoas/:estudanteId/matricula', PessoaController.criarMatricula)
+  .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+  .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
 
 module.exports = router;
